2c548d8b13763ff3532c4b6b215592a46ed7ffd3
shulihallak <shuli@invisiblenetworks.co>
2015-11-01 16:34:09 -0500

    form-based workflow featured completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
 #instance variables allow views to access the variable
 
   def index
@@ -7,5 +8,35 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    flash[:notice]
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    flash[:notice]
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice]
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    redirect_to article_path(@article)
   end
 end
